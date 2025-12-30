4a9d6dbb4d46120e49a3ce62cf7d3be9db5fc261
ssayciao <ssayciao@gmail.com>
2025-12-31 00:58:35 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -48,7 +48,7 @@ const App: React.FC = () => {
       },
       {
         id: '2',
-        url: '/artwork/image2.jpeg',
+        url: '/artwork/8.jpg',
         title: 'Aoiike, Japan',
         medium: 'Soft pastel',
         year: '2024',
